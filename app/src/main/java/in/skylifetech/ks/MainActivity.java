144fb0610ec1e5c309eeedf79c136bc65eb0d3f7
Karthikeyan <karthikeyan.k2000@gmail.com>
2019-03-15 13:23:09 +0530

    Added AlertDialog to check internet connnectivity!

--- app/src/main/java/in/skylifetech/ks/MainActivity.java
+++ app/src/main/java/in/skylifetech/ks/MainActivity.java
@@ -1,8 +1,15 @@
 package in.skylifetech.ks;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 
@@ -17,10 +24,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TabLayout tabLayout = findViewById(R.id.sliding_tabs);
-        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.viewpager);
-        viewPager.setAdapter(adapter);
-        tabLayout.setupWithViewPager(viewPager);
+        if (isNetworkConnectionAvailable()) {
+            TabLayout tabLayout = findViewById(R.id.sliding_tabs);
+            SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
+            ViewPager viewPager = findViewById(R.id.viewpager);
+            viewPager.setAdapter(adapter);
+            tabLayout.setupWithViewPager(viewPager);
+        }
+    }
+
+    public void noNetworkConnection() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Please check your network state!");
+        builder.setMessage("Please trun on internet connection to continuse!");
+        builder.setNegativeButton("close", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                MainActivity.this.startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    public boolean isNetworkConnectionAvailable() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnected();
+        if (isConnected) {
+            return true;
+        } else {
+            noNetworkConnection();
+            return false;
+        }
     }
 }
