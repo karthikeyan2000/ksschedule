9626455f539970142ca04b6d90c12004b0f0f86b
Karthikeyan <karthikeyan.k2000@gmail.com>
2019-03-16 13:03:21 +0530

    Edited AlertDialog to check internet connnectivity!

--- app/src/main/java/in/skylifetech/ks/MainActivity.java
+++ app/src/main/java/in/skylifetech/ks/MainActivity.java
@@ -14,47 +14,38 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (isNetworkConnectionAvailable()) {
-            TabLayout tabLayout = findViewById(R.id.sliding_tabs);
-            SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
-            ViewPager viewPager = findViewById(R.id.viewpager);
-            viewPager.setAdapter(adapter);
-            tabLayout.setupWithViewPager(viewPager);
-        } else
-            noNetworkConnection();
+        CheckNetworkConnection();
+        TabLayout tabLayout = findViewById(R.id.sliding_tabs);
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
-    public void noNetworkConnection() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Please check your network state!");
-        builder.setIcon(R.drawable.no_internet_icon);
-        builder.setMessage("Please trun on internet connection to continue!");
-        builder.setNegativeButton("close", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-                MainActivity.this.startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
-            }
-        });
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-    }
-
-    public boolean isNetworkConnectionAvailable() {
+    public void CheckNetworkConnection() {
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        return activeNetwork != null && activeNetwork.isConnected();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (!isNetworkConnectionAvailable())
-            noNetworkConnection();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnected();
+        if (!isConnected) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Please check your network state!");
+            builder.setIcon(R.drawable.no_internet_icon);
+            builder.setMessage("Please turn on internet connection to continue!");
+            builder.setNegativeButton("SETTINGS", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    MainActivity.this.startActivity(new Intent(Settings.ACTION_NETWORK_OPERATOR_SETTINGS));
+                    dialog.dismiss();
+                }
+            });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        }
     }
 }
